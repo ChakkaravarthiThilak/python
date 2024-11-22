4b57b8d3ce1872a6390ae0584f9e89b1be0506df
ChakkaravarthiThilak <thilakchakkaravarthi@gmail.com>
2024-11-22 17:55:14 +0530

    Add files via upload

---
+++ Adding new dictionary.py
@@ -0,0 +1,8 @@
+import collection
+dict 1={'a':1,'b':2}
+dict 2={'b':3,'c':4}
+dict 3={'f':5}
+chain= collections.ChainMap(dict 1,dict 2)
+print("All the chain map counters are:")
+print (chain)
+Chain1=chain.new_child(dict3
