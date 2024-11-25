f13978b3f09c1fe29682681cf984093ec73af401
ChakkaravarthiThilak <thilakchakkaravarthi@gmail.com>
2024-11-25 17:14:18 +0530

    Add files via upload

---
+++ Nested Statement.py
@@ -0,0 +1,12 @@
+student={
+    "class a":{
+        "name":"hari",
+        "id"      :1304
+      },
+    "class b":{
+        "name":"ajai",
+        "id"     :143
+     }
+    }
+print(student)
+      
