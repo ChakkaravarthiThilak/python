f4248540483ba46b4b2706e41ad41d85f0f5a122
ChakkaravarthiThilak <thilakchakkaravarthi@gmail.com>
2024-11-25 17:05:07 +0530

    Add files via upload

---
+++ Functions as an arguments.py
@@ -0,0 +1,9 @@
+def var (text):
+    return text
+def abc (text):
+    return text
+def bfg(func):
+    a=func("Hi")
+    print(a)
+bfg(var)
+bfg(abc)
---
+++ Functions as an object.py
@@ -0,0 +1,7 @@
+def var(text):
+    return text
+print (var("Hello"))
+abc=var
+print (var("Hello"))
+abc=var
+print (abc("Hi"))
